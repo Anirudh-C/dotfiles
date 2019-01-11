946de227ed28934003f682bbcaad481397259bbe
Anirudh <anirudhvan@gmail.com>
2019-01-11 05:34:52 +0530

    added setup script and script sugar

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+setosconfig() {
+    rm ~/.xinitrc
+    cp OS/xinitrc ~/.xinitrc
+    rm ~/.Xresources
+    cp OS/Xresources ~/.Xresources
+    rm ~/.xmodmap
+    cp OS/Keyboard/xmodmap ~/.xmodmap
+}
+
+settmuxconfig() {
+    rm ~/.tmux.conf
+    cp Work/tmux.conf ~/.tmux.conf
+}
+
+setshellconfig() {
+    rm ~/.zshrc
+    cp Work/Shell/zshrc ~/.zshrc
+    rm -rf ~/.oh-my-zsh
+    cp -r Work/Shell/oh-my-zsh ~/
+    mv ~/oh-my-zsh ~/.oh-my-zsh
+}
+
+setconfigdir() {
+    rm -rf ~/.config
+    cp -r Config/ ~/
+    mv ~/Config/ ~/.config
+}
+
+loading() {
+    mypid=$!
+    loadingText=$1
+
+    echo -ne "$loadingText\r"
+
+    while kill -0 $mypid 2>/dev/null; do
+        echo -ne "$mypid :: $loadingText.\r"
+        sleep 0.5
+        echo -ne "$mypid :: $loadingText..\r"
+        sleep 0.5
+        echo -ne "$mypid :: $loadingText...\r"
+        sleep 0.5
+        echo -ne "\r\033[K"
+        echo -ne "$mypid :: $loadingText\r"
+        sleep 0.5
+    done
+
+    echo "$loadingText...Finished"
+}
+
+setosconfig & loading "Getting OS config files"
+settmuxconfig & loading "Getting tmux config"
+setshellconfig & loading "Getting shell config"
+setconfigdir & loading "Getting .config directory"
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,31 +1,58 @@
 #!/bin/sh
 
-# OS
-cp ~/.xinitrc OS
-mv OS/.xinitrc OS/xinitrc
-cp ~/.Xresources OS
-mv OS/.Xresources OS/Xresources
-cp -r ~/.config/i3 OS
-cp /etc/X11/xorg.conf.d/10-monitor.conf OS/Monitor
-cp ~/.xmodmap OS/Keyboard
-mv OS/Keyboard/.xmodmap OS/Keyboard/xmodmap
+copyosconfig() {
+    cp ~/.xinitrc OS
+    mv OS/.xinitrc OS/xinitrc
+    cp ~/.Xresources OS
+    mv OS/.Xresources OS/Xresources
+    cp -r ~/.config/i3 OS
+    cp /etc/X11/xorg.conf.d/10-monitor.conf OS/Monitor
+    cp ~/.xmodmap OS/Keyboard
+    mv OS/Keyboard/.xmodmap OS/Keyboard/xmodmap
+}
 
-# Tmux
-cp ~/.tmux.conf Work
-mv Work/.tmux.conf Work/tmux.conf
+copytmuxconfig() {
+    cp ~/.tmux.conf Work
+    mv Work/.tmux.conf Work/tmux.conf
+}
 
-# Shell
-cp ~/.zshrc Work/Shell
-mv Work/Shell/.zshrc Work/Shell/zshrc
-cp -r ~/.oh-my-zsh Work/Shell
-rm -rf Work/Shell/oh-my-zsh
-mv Work/Shell/.oh-my-zsh Work/Shell/oh-my-zsh
-rm -rf Work/Shell/oh-my-zsh/.git
+copyshellconfig() {
+    cp ~/.zshrc Work/Shell
+    mv Work/Shell/.zshrc Work/Shell/zshrc
+    cp -r ~/.oh-my-zsh Work/Shell
+    rm -rf Work/Shell/oh-my-zsh
+    mv Work/Shell/.oh-my-zsh Work/Shell/oh-my-zsh
+    rm -rf Work/Shell/oh-my-zsh/.git
+}
 
-# Terminal
-# cp -r ~/.config/alacritty Work
+copyconfigdir() {
+    cp -r ~/.config .
+    rm -rf Config/
+    mv .config/ Config/
+}
 
-# All system config files
-cp -r ~/.config .
-rm -rf Config/
-mv .config/ Config/
+loading() {
+    mypid=$!
+    loadingText=$1
+
+    echo -ne "$loadingText\r"
+
+    while kill -0 $mypid 2>/dev/null; do
+        echo -ne "$loadingText.\r"
+        sleep 0.5
+        echo -ne "$loadingText..\r"
+        sleep 0.5
+        echo -ne "$loadingText...\r"
+        sleep 0.5
+        echo -ne "\r\033[K"
+        echo -ne "$loadingText\r"
+        sleep 0.5
+    done
+
+    echo "$loadingText...Finished"
+}
+
+copyosconfig & loading "Copying OS config files"
+copytmuxconfig & loading "Copying tmux config"
+copyshellconfig & loading "Copying shell config"
+copyconfigdir & loading "Copying .config directory"
