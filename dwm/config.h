04ed17b67351dff30bef4116c5f2e2ddb29422f6
Anirudh C <anirudh.c@iiitb.org>
2019-07-21 18:35:50 +0530

    attempt fix for dpi

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int barheight = 32;
-static const unsigned int lrpadding = 16;
+static const unsigned int barheight = 27;
+static const unsigned int lrpadding = 15;
 static const char *fonts[]          = { "Iosevka:size=9" };
 static const char dmenufont[]       = "Iosevka:size=9";
 
--- term/config.h
+++ term/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:size=8:antialias=true:autohint=true";
+static char *font = "Iosevka:size=9:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
