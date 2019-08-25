7e7b3b9b09ed87d3049b117246f410a133127be0
Anirudh C <anirudh.c@iiitb.org>
2019-08-25 17:49:52 +0530

    automate sink choice in volume.sh

--- volume.sh
+++ volume.sh
@@ -3,25 +3,26 @@
 incr=+5%
 decr=-5%
 
+sink=$(pacmd list-sinks | grep "index" | awk '{print $3}')
 cur=$(pactl list sinks | grep "Volume: " | awk '{print $12}')
 
 case "$1" in
 	"up")
 		if [[ "$cur" == "100%" ]]; then
-			pactl set-sink-volume 0 100%
+			pactl set-sink-volume $sink 100%
 		else
-			pactl set-sink-volume 0 $incr
+			pactl set-sink-volume $sink $incr
 		fi
 	;;
 	"down")
 		if [[ "$cur" == "0%" ]]; then
-			pactl set-sink-volume 0 0%
+			pactl set-sink-volume $sink 0%
 		else
-			pactl set-sink-volume 0 $decr
+			pactl set-sink-volume $sink $decr
 		fi
 	;;
 	"mute")
-		pactl set-sink-mute 0 toggle
+		pactl set-sink-mute $sink toggle
 	;;
 	*)
 		echo "Unsupported: \"$1\""
